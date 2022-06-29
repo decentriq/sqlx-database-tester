6eff1cfc378b5f1820f439d682be16fa69225e0e
Karl Skomski <k.skomski@famedly.com>
2022-06-29 10:37:03 +0200

    chore: Remove unneeded lints in code

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,4 @@
 #![doc = include_str!("../README.md")]
-#![deny(
-	missing_docs,
-	trivial_casts,
-	trivial_numeric_casts,
-	unused_extern_crates,
-	unused_import_braces,
-	unused_qualifications
-)]
-#![warn(missing_debug_implementations, dead_code, clippy::unwrap_used, clippy::expect_used)]
-
 use std::{env, str::FromStr};
 
 pub use dotenv;
